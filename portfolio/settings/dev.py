62df05713ab8aa583f2c11dff2d54fb9a9b82ae9
Thomas Vu <thomas.vu@hotmail.com>
2023-09-23 15:32:26 +0200

    installed mysql

--- portfolio/settings/dev.py
+++ portfolio/settings/dev.py
@@ -6,7 +6,10 @@ SECRET_KEY = 'django-insecure-cr3c54fx4log@6x&&uuwd%m3!$3zd+nlaj+r436q91vs21ogt2
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'portfolio',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'sk8erkul'
     }
 }
